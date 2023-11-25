5f4469f0c22dee8b219b1ca217a0eeac646a8f00
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-26 00:31:39 +0530

    advice generator app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { useEffect, useState } from "react";
 
-export default App
+import {
+  diceIcon,
+  dividerDesktopPattern,
+  dividerMobilePattern,
+} from "./assets/images";
+
+const API = "https://api.adviceslip.com/advice";
+
+const App = () => {
+  const [advice, setAdvice] = useState({});
+
+  const getAdvice = async () => {
+    await fetch(API)
+      .then((response) => response.json())
+      .then((data) => setAdvice(data.slip))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <main className="main">
+      <div className="card">
+        <p className="advice-id">
+          Advice {advice.id}
+        </p>
+        <h3 className="advice">&quot;{advice.advice}&quot;</h3>
+        <picture className="advice-divider">
+          <source media="min-width:780px" srcSet={dividerDesktopPattern} />
+          <img
+            src={dividerMobilePattern}
+            alt="divider-pattern"
+            className="w-full"
+          />
+        </picture>
+        <figure className="dice" onClick={() => getAdvice()}>
+          <img src={diceIcon} alt="dice-icon" />
+        </figure>
+      </div>
+    </main>
+  );
+};
+
+export default App;
