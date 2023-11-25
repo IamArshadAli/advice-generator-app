e1feffca4fb6b6c90811cf2ac8a4f29d62111199
IamArshadAli <arshadalikaldane@gmail.com>
2023-11-26 00:26:58 +0530

    assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import diceIcon from "./icon-dice.svg";
+import dividerDesktopPattern from "./pattern-divider-desktop.svg";
+import dividerMobilePattern from "./pattern-divider-mobile.svg";
+
+export { diceIcon, dividerDesktopPattern, dividerMobilePattern };
